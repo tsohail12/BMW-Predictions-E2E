bcdaa23af8496397fbc2173d7e6df09fb45e1814
tsohail <mdssohail1018@gmail.com>
2026-01-18 20:14:09 +0530

    Data validation has been added to pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from car_price import logger
 from car_price.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
+from car_price.pipeline.stage2_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -12,3 +13,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+    data_ingestion = DataValidationTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
